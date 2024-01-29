f6a5e19e71f85f365a3695d2e4aa9acc8079d41d
alexx13 <104129257+alexxpilaar@users.noreply.github.com>
2024-01-29 19:03:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,5 @@
 import subprocess
 
 def run():
-    # Aquí puedes cambiar el comando que deseas ejecutar en el cmd
-    comando = "whoami"
-    
-    # Ejecutar el comando en el cmd
-    resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    
-    # Imprimir la salida estándar y el error estándar del comando
-    print("Salida estándar:", resultado.stdout)
-    print("Error estándar:", resultado.stderr)
-
-# Llamamos a la función run()
+  print('Hola Mundo')
 run()
