19f9b221f69369e1d57f678763d80eb6e4ddf7e3
alexx13 <104129257+alexxpilaar@users.noreply.github.com>
2024-01-31 15:30:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
+
 import subprocess
 
 def run():
-  print('Hola Mundo')
+    
+    comando = "whoami"
+    
+    
+    resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    
+    print("Salida estándar:", resultado.stdout)
+    print("Error estándar:", resultado.stderr)
+
 run()
